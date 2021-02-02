530bc5eb90acd6e45941470884f1d713029972dc
Ventseslava Shopova <supervenca@yandex.ru>
2021-02-02 23:46:53 +0200

    js added

---
+++ script.js
@@ -0,0 +1,14 @@
+document.querySelector("form").addEventListener("submit", handleSubmit);
+
+const handleSubmit = (e) => {
+    e.preventDefault();
+    let myForm = document.querySelector('form');
+    let formData = new FormData(myForm);
+
+    fetch('/', {
+        method: 'POST',
+        headers: { "Content-Type": "application/x-www-form-urlencoded" },
+        body: new URLSearchParams(formData).toString()
+    }).then(() => console.log('Form successfully submitted')).catch((error) =>
+        alert(error))
+}
